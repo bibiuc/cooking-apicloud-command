011dee0ae082118120626cf506a54b051d4d8913
bbuc <bbuc@qq.com>
2017-01-17 16:44:50 +0800

    setting

--- watch.js
+++ watch.js
@@ -6,19 +6,33 @@ const webpack = require('webpack')
 const APICloud = require('apicloud-tools-core');
 
 
-module.exports = function (program, configs) {
-	  let compiler;
-	  configs.devServer.enable = false;  
-	  compiler = webpack(configs)
+module.exports = function(program, configs) {
+    let compiler;
+    configs.devServer.enable = {
+        port: 8080,
+        hot: true,
+        log: true,
+        enable: false,
+        historyApiFallback: true,
+        lazy: false,
+        stats: 'errors-only',
+        host: '0.0.0.0',
+        __host__: 'http://0.0.0.0:8080'
+    };
+    compiler = webpack(configs)
 
-	  logger.log('Loading...')
-	  compiler.watch({}, (err, stats) => {
-		      if (err) {
-			            return logger.error(err)
-			          }
-		      APICloud.syncWifi({syncAll:false, projectPath:compiler.options.output.path})
-		      logger.log('webpack info \n' + stats.toString(configs.devServer.stats))
-		    })
-	  APICloud.startWifi({port:program.progress || 8090})
+    logger.log('Loading...')
+    compiler.watch({}, (err, stats) => {
+        if (err) {
+            return logger.error(err)
+        }
+        APICloud.syncWifi({
+            syncAll: false,
+            projectPath: compiler.options.output.path
+        })
+        logger.log('webpack info \n' + stats.toString(configs.devServer.stats))
+    })
+    APICloud.startWifi({
+        port: program.progress || 8090
+    })
 };
-
