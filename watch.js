c68e46d14fc6f7f70ff0d033d95b1bc9ad61d2d3
bbuc <bbuc@qq.com>
2017-01-17 11:33:32 +0800

    watch

--- watch.js
+++ watch.js
@@ -7,17 +7,18 @@ const APICloud = require('apicloud-tools-core');
 
 
 module.exports = function (program, configs) {
-  let compiler;
-  configs.devServer = {enable:false};
-  compiler = webpack(configs)
+	  let compiler;
+	  configs.devServer.enable = false;  
+	  compiler = webpack(configs)
 
-  logger.log('Loading...')
-  compiler.watch({}, (err, stats) => {
-    if (err) {
-      return logger.error(err)
-    }
-    APICloud.syncWifi({syncAll:false, projectPath:compiler.options.output.path})
-    logger.log('webpack info \n' + stats.toString(configs.devServer.stats))
-  })
-  APICloud.startWifi({port:program.progress || 8090})
+	  logger.log('Loading...')
+	  compiler.watch({}, (err, stats) => {
+		      if (err) {
+			            return logger.error(err)
+			          }
+		      APICloud.syncWifi({syncAll:false, projectPath:compiler.options.output.path})
+		      logger.log('webpack info \n' + stats.toString(configs.devServer.stats))
+		    })
+	  APICloud.startWifi({port:program.progress || 8090})
 };
+
