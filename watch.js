d05a5731cd0e4dfc34f1a320309173fe1a4f44ac
bbuc <bbuc@qq.com>
2017-01-17 18:44:20 +0800

    devServer

--- watch.js
+++ watch.js
@@ -8,7 +8,17 @@ const APICloud = require('apicloud-tools-core');
 
 module.exports = function(program, configs) {
     let compiler;
-    configs.devServer.enable = false;
+    configs.devServer = {
+        port: 8080,
+        hot: true,
+        log: true,
+        enable: false,
+        historyApiFallback: true,
+        lazy: false,
+        stats: 'errors-only',
+        host: '0.0.0.0',
+        __host__: 'http://0.0.0.0:8080'
+    };
     compiler = webpack(configs)
 
     logger.log('Loading...')
